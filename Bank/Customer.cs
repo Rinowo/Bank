4ef5aad37b2718368baed13086a50b5ac065fd13
Rinowo <tunganhhh205@gmail.com>
2022-08-22 09:40:19 +0700

    final

---
+++ Bank/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Customer
+    {
+        private string name;
+        private int age;
+
+        List<BankAccount> accounts;
+
+        public Customer(string name, int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = accounts;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
